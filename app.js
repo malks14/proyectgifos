2978321f16d1da9d3028ab227373575cf4a46c1b
malks14 <greco.guido@hotmail.com>
2020-12-21 17:26:36 -0300

    hamburguesa

---
+++ app.js
@@ -0,0 +1,9 @@
+function darkMode() {
+    var element = document.getElementById('main');
+    element.classList.toggle("dark-mode");
+ }
+
+ function changeClass() {
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+ }
