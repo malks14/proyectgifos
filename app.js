2b7c8dc38d1c42f10185fcaf06cf2d8028999680
malks14 <greco.guido@hotmail.com>
2020-12-21 23:59:29 -0300

    footer y mas

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+// aggregate toggle functions
+
 function darkMode() {
     darkHeader();
     darkMain();
+    darkTrending();
+    darkFooter();
 }
 
+//dayMode
 
+function dayTrending() {
+    var element = document.getElementById('trending');
+    element.classList.toggle("day-modeTrending");
+}
+
+// darkMode
+function darkTrending() {
+    var element = document.getElementById('trending');
+    element.classList.toggle("dark-modeTrending");
+}
 
 function darkMain() {
     var element = document.getElementById('main');
@@ -15,6 +30,12 @@ function darkHeader() {
     elementTwo.classList.toggle("dark-mode")
 }
 
+function darkFooter() {
+    var element = document.getElementById('footer');
+    element.classList.toggle("dark-mode");
+}
+
+//menuhamburger
 
 function changeClass() {
     let siteNav = document.getElementById('site-nav');
