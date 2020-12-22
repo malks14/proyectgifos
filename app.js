3fa318a72ca44191898dda4e431e6f92172167d8
malks14 <greco.guido@hotmail.com>
2020-12-21 22:22:15 -0300

    placeholder

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 function darkMode() {
+    darkHeader();
+    darkMain();
+}
+
+
+
+function darkMain() {
     var element = document.getElementById('main');
     element.classList.toggle("dark-mode");
- }
+}
 
- function changeClass() {
+function darkHeader() {
+    var elementTwo = document.getElementById('headerDark')
+    elementTwo.classList.toggle("dark-mode")
+}
+
+
+function changeClass() {
     let siteNav = document.getElementById('site-nav');
         siteNav.classList.toggle('site-nav-open');
- }
+    
+}
