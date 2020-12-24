e607809404f3f55c1e1917160771eee3106cae3d
malks14 <greco.guido@hotmail.com>
2020-12-24 19:50:48 -0300

    nuevo header fav

--- app.js
+++ app.js
@@ -51,12 +51,32 @@ function changeClass() {
     
 }
 
-// changinli
+const navSlide = () => {     //asi es una funcion vacia
+    const burger =  document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
 
-    // window.onload = function changeLi(){
-    // document.getElementById("dayLi").innerHTML = "Modo Diurno";
+    //Toggle nav
+    burger.addEventListener('click', ()=>{ //funcion para que el boton tome la clase de active
+        nav.classList.toggle('nav-active');
 
-    // window.onload = function changeLi(){
-    // document.getElementById("dayLi").innerHTML = "Modo Diurno";
+     //Animacion li (links)
+    // navLinks.forEach((link, index) => {  //funcion para que quede siempre la animacion de li por segundos
+    //     if(link.style.animation) {
+    //         link.style.animation = ''
+    //     } else {
+    //      link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 1.5 }s`; //animacion para que los li aparezcan de a poco
+    //     }
+         
+     
+    //  });
+     //animacion burger
+     burger.classList.toggle('toggle');
+
+    });
+   
+}
+
+navSlide(); //para llamar a la funcion
 
