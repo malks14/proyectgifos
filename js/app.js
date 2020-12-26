c052144b2db20cb58e6208b9c8a45da5cebf2315
malks14 <greco.guido@hotmail.com>
2020-12-26 20:21:10 -0300

    trending gif

---
+++ js/app.js
@@ -0,0 +1,82 @@
+// aggregate toggle functions
+
+function darkMode() {
+    darkHeader();
+    darkMain();
+    darkTrending();
+    darkFooter();
+    darkBtn();
+}
+
+//dayMode
+function dayMode() {
+    dayTrending();
+    
+}
+
+function dayTrending() {
+    var element = document.getElementById('trending');
+    element.classList.toggle("day-modeTrending");
+}
+
+// darkMode
+function darkTrending() {
+    var element = document.getElementById('trending');
+    element.classList.toggle("dark-modeTrending");
+}
+
+function darkMain() {
+    var element = document.getElementById('main');
+    element.classList.toggle("dark-mode");
+}
+
+function darkHeader() {
+    var elementTwo = document.getElementById('headerDark')
+    elementTwo.classList.toggle("dark-mode")
+    
+}
+
+function darkFooter() {
+    var element = document.getElementById('footer');
+    element.classList.toggle("dark-mode");
+}
+
+function darkBtn() {
+    var element = document.getElementById('burger');
+    element.classList.toggle("dark-mode");
+}
+
+function darkSearch() {
+    var element = document.getElementById('searchbar');
+    element.classList.toggle("dark-mode");
+}
+//menuhamburger
+
+function changeClass() {
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+    
+}
+
+const navSlide = () => {     
+    const burger =  document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    //Toggle nav
+    burger.addEventListener('click', ()=>{ //funcion para que el boton tome la clase de active
+        nav.classList.toggle('nav-active');
+
+         
+     
+     
+     //animacion burger
+     burger.classList.toggle('toggle');
+
+    });
+   
+}
+
+navSlide(); 
+
---
+++ js/trending.js
@@ -0,0 +1,31 @@
+const apikey = 'ndufihd3YVOMrmoIEogAJJ2vZ8Ysk9mK';
+const path = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}`;
+
+fetch(path).then(function(res) {
+    return res.json()
+}).then(function(json) {
+    console.log(json.data[0].images.fixed_width.url)
+    const trendingEl = document.getElementById('trending_gif')
+    let trendingHTML = ''
+
+
+    json.data.forEach(function(obj) {
+        console.log(obj.images.fixed_width)
+
+        const url = obj.images.fixed_width.url
+        const width = obj.images.fixed_width.width
+        const height = obj.images.fixed_width.height
+        const title = obj.images.fixed_width.title
+        trendingHTML += `<img 
+            class="display_gif"
+            src="${url}"
+            width="${width}" 
+            height="${height}" 
+            alt="${title}">`
+
+    });
+
+    trendingEl.innerHTML = trendingHTML
+}).catch(function(err) {
+    console.log(err.message)
+})
