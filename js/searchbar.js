8c02623198623534752b6bcbe94ed97198996209
malks14 <greco.guido@hotmail.com>
2021-02-05 16:54:18 -0300

    arreglos codigo

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,45 +1,104 @@
 
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById('search-input');
+const trendingEl = document.getElementById('searchResults')
+const seatchBtn = document.getElementById('searchBtn');
+
 //probar con searchnput para ver si funciona con lo que pone el usaurio arriba de los gif TAREA
 
-searchForm.addEventListener('submit', function(e) { //e porque siempre submit tiene un event
-    e.preventDefault() //es para que que no se reload los gif
-    const q = searchInput.value; //esto es lo que el usuario escriba
-    let search_term = document.getElementById("searchTerm")
-    search_term.innerHTML = q;
+// searchForm.addEventListener('submit', function(e) { //e porque siempre submit tiene un event
+//     e.preventDefault() //es para que que no se reload los gif
+//     const q = searchInput.value; //esto es lo que el usuario escriba
+//     let search_term = document.getElementById("searchTerm")
+//     search_term.innerHTML = q;
     
-    search(q)
-})
+//     search(q)
+// })
 //esta funcion es para cuando se ejecute es porque el usuario busco en la barra
 
 
-function search(q) { //funcion que busca los gifs. Se pone como parametro q porque es lo que va a escribir el usuario. Por eso tambien el path est adentro de la funcion
+// function search(q) { //funcion que busca los gifs. Se pone como parametro q porque es lo que va a escribir el usuario. Por eso tambien el path est adentro de la funcion
 
-    const pathSearch = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`;
+//     
    
-    fetch(pathSearch).then(function(res) {
-        return res.json()
-    }).then(function(json) {
-        console.log(json.data[0].embed_url)
-        const trendingEl = document.getElementById('searchResults')
-        let trendingHTML = ''
+//     fetch(pathSearch).then(function(res) {
+//         return res.json()
+//     }).then(function(json) {
+//         console.log(json.data[0].embed_url)
+//         
+//         let trendingHTML = ''
     
     
-        json.data.forEach(function(obj) {
-            console.log(obj) 
+//         json.data.forEach(function(obj) {
+//             console.log(obj) 
     
-            const url = obj.images.fixed_width.url
-            const title = obj.images.title
-            trendingHTML += `<img 
-                class="display_search"
-                src="${url}"
-                alt="${title}">`
+//             const url = obj.images.fixed_width.url
+//             const title = obj.images.title
+//             trendingHTML += `<img 
+//                 class="display_search"
+//                 src="${url}"
+//                 alt="${title}">`
     
-        });
+//         });
     
-        trendingEl.innerHTML = trendingHTML
-    }).catch(function(err) {
-        console.log(err.message)
+//         trendingEl.innerHTML = trendingHTML
+//     }).catch(function(err) {
+//         console.log(err.message)
+//     })
+// }
+function search() {
+    async function newsSearch(q) {
+        const pathSearch = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`;
+        console.log(pathSearch);
+        const resp = await fetch(pathSearch);
+        const info = await resp.json();
+        return info;
+    }
+    
+    let info = newsSearch(searchInput.value)
+    info.then(data => {
+        console.log(data)
+        // arraySearch = [];
+        // class gifSearch {
+        //     constructor(src) {
+        //     this.src = src;
+        //       }
+        //     }
+
+        for (i = 0; i < 12; i++) {
+            // let newSrc = data[i].images.fixed_width.url;
+           
+            // let giftTrendings = new gifSearch(
+            //     newSrc
+            // );
+            // arraySearch.push(giftTrendings);
+            let img = document.createElement("img");
+            img.setAttribute("src", data[i].images.fixed_width.url);
+            // img.src = newSrc;
+            trendingEl.appendChild(img);
+        }
+        
+
+    }).catch(err => {
+        console.log("error", err);
     })
+    
 }
+
+seatchBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    const q = searchInput.value;
+    let search_term = document.getElementById("searchTerm")
+    search_term.innerHTML = q;
+    search();
+})
+searchForm.addEventListener('keyup', (e)=> {
+    e.preventDefault()
+    if (event.which === 13 || event.keyCode == 13) {
+        const q = searchInput.value;
+        let search_term = document.getElementById("searchTerm")
+        search_term.innerHTML = q;
+        search();
+    }
+    
+})
--- js/trendingGif.js
+++ js/trendingGif.js
@@ -1,34 +1,144 @@
 //Trending Gif
-
+let limit = 3;
+let offset = 0;
 const apikey = 'ndufihd3YVOMrmoIEogAJJ2vZ8Ysk9mK';
-const path = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}`;
 
-fetch(path).then(function(res) {
-    return res.json()
-}).then(function(json) {
-    console.log(json.data[0].embed_url)
-    const trendingEl = document.getElementById('trending_gif')
-    let trendingHTML = ''
+const gifTrendingCtn = document.getElementById('gifTrendingCtn');
+const trendinginfo = document.getElementById('trendinginfo');
+const path = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=${limit}&offset=${offset}`;
 
 
-    json.data.forEach(function(obj) {
-        console.log(obj) 
+    
+    fetch(path).then((response => {
+        return response.json();
+    })).then((json => {
+         
+        console.log(json.data)
+        arrayTrending = [];
+        class gifTrendings {
+            constructor(src, title) {
+            this.src = src;
+            this.title = title;
+              }
+            }
+            
+        for (i = 0; i < limit; i++) {
+            if (screen.width > 768) {
+                limit = 3;
+            } else {
+                limit = 24;
+            }
+    
+            let newSrc = json.data[i].images.fixed_width.url;
+            let newTitle = json.data[i].title;
+            let giftTrendings = new gifTrendings(
+                newSrc,
+                newTitle,
+            );
+                
+            arrayTrending.push(giftTrendings);
+              
+            let img = document.createElement("img");
+           
+            img.setAttribute("class", "trending_gif");
+            img.setAttribute("class", "display_gif");
+            img.src = newSrc;
+            img.title = newTitle;
+            gifTrendingCtn.appendChild(img);
+            
+        }
+        let btnR = document.createElement("button");
+        btnR.setAttribute("id", "arrowRight")
+        btnR.setAttribute("class", "fas fa-chevron-right");
+        gifTrendingCtn.appendChild(btnR);
 
-        const url = obj.images.fixed_width.url;
-        const title = obj.images.title
-        trendingHTML += `<img 
-            id = "displayMax"
-            class="display_gif"
-            onclick="displayMax(this)"
-            src="${url}"
-            alt="${title}">`
+        btnR.addEventListener("click", function () {
+            if (screen.width > 1280) {
+                document.getElementById("gifTrendingCtn").scrollLeft += 1158;
+            }
+        });
+    
+    })).catch(err => {
+        console.log("error", err);
+    })
 
-    });
 
-    trendingEl.innerHTML = trendingHTML
-}).catch(function(err) {
-    console.log(err.message)
-})
+   
+
+
+    
+
+
+
+    // async function trending() {
+    //     console.log(path);
+    //     const resp = await fetch(path);
+    //     const info = await  resp.json();
+    //     return info;
+        
+    // }
+
+    // let info = trending(info);
+    // console.log(info);
+    // info.then(data => {
+    //     console.log(info);
+    //     let arrayTrending = [];
+    //     class gifTrending {
+    //         constructor (src,title) {
+    //             this.src = src;
+    //             this.title = title;
+    //         }
+    //     }
+
+    //     for (i = 0; i < 24; i++) {
+    //         let newSrc = json.data[i].images.fixed_width.url;
+    //         let newTitle = json.data[i].title;
+    //         let giftTrendings = new gifTrendings(
+    //           newSrc,
+    //           newTitle,
+    //         );
+    //         arrayTrending.push(giftTrendings);
+
+    //         let img = document.createElement("img");
+    //         img.setAttribute("class", "trending_gif");
+    //         img.setAttribute("class", "display_gif");
+    //         img.src = newSrc;
+    //         img.title = newTitle;
+            
+    //         gifTrendingCtn.appendChild(img);
+    //     }
+    // }).catch(err => {
+    //         console.log("error", err);
+    // })
+
+
+
+
+// fetch(path).then(function(res) {
+//     return res.json()
+// }).then(function(json) {
+//     console.log(json.data[0].embed_url)
+//     const trendingEl = document.getElementById('trending_gif')
+//     let trendingHTML = []
+
+
+//     json.data.forEach(function(obj) {
+//         console.log(obj) 
+
+//         const url = obj.images.fixed_width.url;
+//         const title = obj.images.title
+//         trendingHTML += `<img 
+//             id = "displayMax"
+//             class="display_gif"
+//             src="${url}"
+//             alt="${title}">`
+
+//     });
+
+//     trendingEl.innerHTML = trendingHTML
+// }).catch(function(err) {
+//     console.log(err.message)
+// })
 
 //Increase size Gif 
 
